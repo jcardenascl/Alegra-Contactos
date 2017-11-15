93f42c3305e4f3bd14ddd6e92cac7de12e85d55c
jose <josecardenas.hostienda@gmail.com>
2017-11-15 15:11:44 -0400

    Version revisada y mejorada

--- www/app/preview-controller.js
+++
@@ -1,8 +0,0 @@
-angular.module('vardyger')
-.controller('PreviewController',
-  function(
-    $scope,  // inject the $scope service
-    post     // inject the resolved post data
-  ) {
-    $scope.item = posts;
-  });
